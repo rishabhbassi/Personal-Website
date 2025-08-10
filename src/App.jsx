61f000eaf3b28872c4559a2fc22e802b61f01bcf
Rishabh Bassi <rishabhbassi0@gmail.com>
2025-08-10 16:08:48 +0530

    fixed loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import React, { useState } from "react";
 import Loader from "./components/Loader";
 import Hero from "./components/Hero";
-import HomePage from "./components/HomePage"; // ⬅️ Add this line
+import HomePage from "./components/HomePage";
 
 export default function App() {
   const [loadingDone, setLoadingDone] = useState(false);
 
   return (
     <>
+      {/* Hero and HomePage are always mounted */}
+      <Hero />
+      <HomePage />
+
+      {/* Loader overlays everything, removed only after animation */}
       {!loadingDone && <Loader onFinish={() => setLoadingDone(true)} />}
-      {loadingDone && (
-        <>
-          <Hero />
-          <HomePage /> {/* ⬅️ Add this below Hero */}
-        </>
-      )}
     </>
   );
 }
